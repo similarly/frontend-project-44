51734c0f0580060e25093b12fa6c9ff8ac2b53d6
Danil <puggermugger@retard-pc.Home>
2023-01-12 02:35:19 +0700

    added prettier and eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import * as cli from './cli.js' 
+import * as cli from './cli.js'
 
 cli.greeting()
--- bin/cli.js
+++ bin/cli.js
@@ -3,9 +3,9 @@ import readLine from 'readline-sync'
 const PROMPT = '>'
 
 function greeting() {
-    console.log('Welcome to the Brain Games!')
-    let userName = readLine.question(`May I ask your name?\n${PROMPT}`)
-    console.log(`Hello, ${userName}!`)
+  console.log('Welcome to the Brain Games!')
+  const userName = readLine.question(`May I ask your name?\n${PROMPT}`)
+  console.log(`Hello, ${userName}!`)
 }
 
 export { greeting }
