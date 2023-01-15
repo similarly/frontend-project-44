0de4ed5f5851339d682c789513e0b9added5ac39
Danil Kalistov <simr0xxorz@gmail.com>
2023-01-16 02:17:10 +0700

    style: linting

--- bin/cli.js
+++ bin/cli.js
@@ -4,5 +4,5 @@ export function greet() {
   console.log('Welcome to the Brain Games!');
   const userName = readLine.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  return userName
+  return userName;
 }
--- bin/index.js
+++ bin/index.js
@@ -3,41 +3,41 @@ import { greet } from './cli.js';
 
 export function run(game) {
   let wins = 0;
-  const userName = greet()
-  const TRIES = 3
-  const NEEDED_WINS = 3
+  const userName = greet();
+  const TRIES = 3;
+  const NEEDED_WINS = 3;
   function roundStart() {
-      /* set new random value */
-      game.newValue();
-      /* ask and get answer */
-      console.log(`Question: ${game.textValue}`);
-      const userAnswer = readLine.question(`Your answer: `);
-      /* check answer correctness */
-      if (game.isCorrect(userAnswer)) {
-        console.log('Correct!');
-        return true;
-      }
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${game.correctAnswer}'.`
-      );
-      console.log(`Let's try again, ${userName}!`);
-      return false;
-    };
-    
-    /* initiate game */
-    console.log(game.startingText);
-    /* run 3 rounds */
-    for (let i = 0; i < TRIES; i += 1) {
-      if (roundStart()) {
-        wins += 1;
-      } else {
-        break;
-      }
+    /* set new random value */
+    game.newValue();
+    /* ask and get answer */
+    console.log(`Question: ${game.textValue}`);
+    const userAnswer = readLine.question('Your answer: ');
+    /* check answer correctness */
+    if (game.isCorrect(userAnswer)) {
+      console.log('Correct!');
+      return true;
     }
-    /* check amount of wins */
-    if (wins === NEEDED_WINS) {
-      console.log(`Congratulations, ${userName}!`);
+    console.log(
+      `'${userAnswer}' is wrong answer ;(. Correct answer was '${game.correctAnswer}'.`
+    );
+    console.log(`Let's try again, ${userName}!`);
+    return false;
+  }
+
+  /* initiate game */
+  console.log(game.startingText);
+  /* run 3 rounds */
+  for (let i = 0; i < TRIES; i += 1) {
+    if (roundStart()) {
+      wins += 1;
+    } else {
+      break;
     }
+  }
+  /* check amount of wins */
+  if (wins === NEEDED_WINS) {
+    console.log(`Congratulations, ${userName}!`);
+  }
 }
 // export const session = {
 //   NEEDED_WINS: 3,
