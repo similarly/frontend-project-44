111352cbc743eac91e91236436c75269dcd12663
Danil Kalistov <simr0xxorz@gmail.com>
2023-01-16 02:03:45 +0700

    text changes

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,6 @@ import readLine from 'readline-sync';
 
 export function greet() {
   console.log('Welcome to the Brain Games!');
-  this.userName = readLine.question('May I ask your name?\n');
+  this.userName = readLine.question('May I have your name? ');
   console.log(`Hello, ${this.userName}!`);
 }
